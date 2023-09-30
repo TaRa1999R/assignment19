ea490f6e8b2d9927aa260cba9374b3c10f74218c
TaRa1999R <rezaietara78@gmail.com>
2023-10-01 00:56:53 +0330

    assignment19

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,2 +1,17 @@
 
-import r
+import random
+import sys
+from PySide6.QtWidgets import QApplication , QMainWindow , QMessageBox
+from ui_password_generator import Ui_MainWindow
+
+class MainWindow ( QMainWindow ) :
+
+    def __init__( self ) :
+        super().__init__()
+        self.ui = Ui_MainWindow ()
+        self.ui.setupUi (self)
+
+app = QApplication ( sys.argv )
+window = MainWindow ()
+window.show ()
+app.exec ()
